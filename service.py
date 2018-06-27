0e70248d773704db10340fec040d0a3b65e43043
Dan Kolbman <dankolbman@gmail.com>
2018-06-27 11:12:33 -0400

    :art: Format messages

--- service.py
+++ service.py
@@ -15,13 +15,53 @@ def handler(event, context):
     if 'Sns' not in event['Records'][0]:
         return
 
-    message = json.loads(event['Records'][0]['Sns']['Message'])
-
+    ev = json.loads(event['Records'][0]['Sns']['Message'])
     color = {
         'info': 'good',
         'warning': 'warning',
         'error': 'danger'
     }
+    
+    attachments = []
+    slack_msg = {
+        'username': 'Release Bot',
+        'icon_emoji': ':calendar:'
+    }
+    emoji = {
+        'info': ":small_blue_diamond:",
+        'error': ":bangbang:",
+        'warning': ":warning:"
+    }
+    message = f"{emoji[ev['event_type']]} {ev['message']}"
+    
+    attachment = {
+        "fallback": ev['message'],
+        "color": color[ev['event_type']],
+        "text": message
+    }
+    
+    author = ''
+    if ev['release'] is not None:
+        author += f":pushpin: {ev['release']}"
+        
+    if ev['task_service'] is not None:
+        author += f" :factory: {ev['task_service']}"
+        
+    if ev['task'] is not None:
+        author += f" :hammer_and_wrench: {ev['task']}"
+
+    # Ignore all task and task service events that are not errors
+    if ev['event_type'] != 'error' and ev['task'] is not None and ev['task_service'] is not None:
+        return
+    
+    if author != '':
+        attachment["author_name"] = author
+        
+    attachments.append(
+        attachment
+    )
+    
+    slack_msg['attachments'] = attachments
 
 
     if 'SLACK_TOKEN' in os.environ and 'SLACK_CHANNEL' in os.environ:
@@ -32,18 +72,7 @@ def handler(event, context):
         SLACK_CHANNEL = [c.replace('#','').replace('@','') for c in SLACK_CHANNEL]
 
         for channel in SLACK_CHANNEL:
-            slack_msg = {
-                'username': 'Release Bot',
-                'icon_emoji': ':calendar:',
-                'channel': channel,
-                'attachments': [
-                    {
-                        "fallback": message['message'],
-                        #"color": color[ev['event_type']],
-                        "text": message['message'],
-                    }
-                ]
-            }
+            slack_msg['channel'] = channel
 
             resp = requests.post('https://slack.com/api/chat.postMessage',
                 headers={'Authorization': 'Bearer '+SLACK_TOKEN},
